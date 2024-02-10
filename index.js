0b99534776dc420fe1d349f2664e6b37d0b39f36
Patryk Oltuch <patryk.oltuch@datacourage.com>
2024-02-10 15:47:36 +0100

    Added websockets for project.

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const http = require('http');
+const { WebSocketServer } = require('ws');
 
 const server = http.createServer();
+const wsServer = new WebSocketServer({ server });
 const port = 8000;
+
+wsServer.on("connection", (connection, request) => {
+    // ws://localhost:8000?username=Alex
+
+    const { username } = url.parse(request.url, true).query
+    console.log(username);
+    
+})
 
 server.listen(port, () => {
     console.log('listening on port ' + port	)
