fd2e2e7a13d17e377a54a249d573af0355143e58
patryk-olti <95oltuchpatryk@gmail.com>
2024-02-12 19:21:05 +0100

    Added get x, y position to backend.

--- index.js
+++ index.js
@@ -1,15 +1,56 @@
 const http = require('http');
 const { WebSocketServer } = require('ws');
 
+const url = require('url');
+const uuidv4 = require('uuid').v4;
+
 const server = http.createServer();
 const wsServer = new WebSocketServer({ server });
 const port = 8000;
+
+const connections = {};
+const users = {};
+
+const broadcast = () => {
+    Object.keys(connections).forEach( uuid => {
+        const connection = connections[uuid]
+        const message = JSON.stringify(users)
+        connection.send(message)
+    })
+}
+
+const handleMessage = (bytes, uuid) => {
+    const message = JSON.parse(bytes.toString())
+    const user = users[uuid]
+    user.state = message
+
+    broadcast()
+
+    console.log(message)
+}
+
+const handleClose = uuid => {
+    
+}
 
 wsServer.on("connection", (connection, request) => {
     // ws://localhost:8000?username=Alex
 
     const { username } = url.parse(request.url, true).query
-    console.log(username);
+    const uuid = uuidv4();
+
+    console.log(username)
+
+    //broadcast
+    connections[uuid] = connection;
+
+    users[uuid] = {
+        username,
+        state: { }
+    }
+
+    connection.on("message", message => handleMessage(message, uuid))
+    connection.on("close", () => handleClose(uuid))
 
 })
 
